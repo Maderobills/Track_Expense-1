11fff4161d487276d424aa8df14e15cfd3bf59ec
Maderobills <96554416+Maderobills@users.noreply.github.com>
2023-01-23 16:27:18 +0000

    Solved signIn_2 issues + startInterface UI

--- app/src/main/java/com/example/trackexpense/Dashboard.java
+++ app/src/main/java/com/example/trackexpense/Dashboard.java
@@ -2,13 +2,38 @@ package com.example.trackexpense;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Dashboard extends AppCompatActivity {
+
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+
+        name=findViewById(R.id.textView10);
+
+        //SignOut
+        FirebaseAuth.getInstance();
+        name.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+
+        findViewById(R.id.sOut).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(Dashboard.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
