51f8e8c5318402bd26ced1744039ad90cc723c70
Maderobills <96554416+Maderobills@users.noreply.github.com>
2023-01-23 20:34:23 +0000

    link loader

--- app/src/main/java/com/example/trackexpense/Dashboard.java
+++ app/src/main/java/com/example/trackexpense/Dashboard.java
@@ -1,39 +1,153 @@
 package com.example.trackexpense;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Dashboard extends AppCompatActivity {
 
-    TextView name;
+    private Toolbar toolbar;
+    private TextView amountView;
+    private RecyclerView recyclerView;
+    private FloatingActionButton fab;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference ref;
+    private String onLineUserId;
+    private ProgressDialog loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Today's Records");
 
+        amountView = findViewById(R.id.totalSpent);
+        recyclerView = findViewById(R.id.recView);
+        fab = findViewById(R.id.fab);
 
-        name=findViewById(R.id.textView10);
-
-        //SignOut
-        FirebaseAuth.getInstance();
-        name.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
-
-        findViewById(R.id.sOut).setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        onLineUserId = mAuth.getCurrentUser().getUid();
+        ref = FirebaseDatabase.getInstance().getReference().child("expenes").child(onLineUserId);
+        loader = new ProgressDialog(this);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(Dashboard.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                addItemSpentOn();
             }
         });
+        
+
+    }
+
+    private void addItemSpentOn() {
+
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        View myView = inflater.inflate(R.layout.input_layout, null);
+        myDialog.setView(myView);
+
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+
+        final Spinner expeneSpinner = myView.findViewById(R.id.spinner);
+        ArrayAdapter<String> expeneAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.expenses));
+        expeneAdapter.setDropDownViewResource(com.google.android.material.R.layout.support_simple_spinner_dropdown_item);
+        expeneSpinner.setAdapter(expeneAdapter);
+
+        final EditText amount = myView.findViewById(R.id.amount);
+        final EditText notes = myView.findViewById(R.id.note);
+        final Button saveBtn = myView.findViewById(R.id.save);
+        final Button cancelBtn = myView.findViewById(R.id.cancel);
+
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mAount = amount.getText().toString();
+                String mNotes = notes.getText().toString();
+                String item = expeneSpinner.getSelectedItem().toString();
+
+                if (mAount.isEmpty()){
+                    amount.setError("Amount required!");
+                    return;
+                }
+                if (mNotes.isEmpty()){
+                    notes.setError("Note required!");
+                    return;
+                }
+                if (item.equals("select item")){
+                    Toast.makeText(Dashboard.this, "Select Item", Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    loader.setMessage("Adding expenses to database");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    String id = ref.push().getKey();
+
+                    DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                    Calendar cal = Calendar.getInstance();
+                    String date = dateFormat.format(cal.getTime());
+
+                    Data data = new Data(item, date, id, mNotes, Integer.parseInt(mAount));
+                    ref.child(id).setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(Dashboard.this, "Expense Added to Database", Toast.LENGTH_SHORT).show();
+
+                            }else {
+                                Toast.makeText(Dashboard.this, "Failed to Add to Database", Toast.LENGTH_SHORT).show();
+                            }
+
+                            loader.dismiss();
+                        }
+                    });
+                }
+                loader.dismiss();
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 }
