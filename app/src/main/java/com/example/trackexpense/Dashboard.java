2d543e390926a7ffb2d08bb4db4a59b73a4cdf33
Maderobills <96554416+Maderobills@users.noreply.github.com>
2023-01-30 03:28:32 +0000

    retrieve data_part1

--- app/src/main/java/com/example/trackexpense/Dashboard.java
+++ app/src/main/java/com/example/trackexpense/Dashboard.java
@@ -3,6 +3,7 @@ package com.example.trackexpense;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,12 +25,19 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -41,18 +50,20 @@ public class Dashboard extends AppCompatActivity {
     private DatabaseReference ref;
     private String onLineUserId;
     private ProgressDialog loader;
+    private TodaysItemsAdapter todaysItemsAdapter;
+    private List<Data> myDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        toolbar = findViewById(R.id.toolbar2);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Today's Records");
 
         amountView = findViewById(R.id.totalSpent);
-        recyclerView = findViewById(R.id.recView);
+
         fab = findViewById(R.id.fab);
 
         mAuth = FirebaseAuth.getInstance();
@@ -65,8 +76,58 @@ public class Dashboard extends AppCompatActivity {
                 addItemSpentOn();
             }
         });
-        
 
+        recyclerView = findViewById(R.id.recView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        myDataList = new ArrayList<>();
+        todaysItemsAdapter = new TodaysItemsAdapter(Dashboard.this,myDataList);
+        recyclerView.setAdapter(todaysItemsAdapter);
+
+        readItems();
+
+    }
+    private void readItems(){
+
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar cal = Calendar.getInstance();
+        String date = dateFormat.format(cal.getTime());
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("expenses").child(onLineUserId);
+        Query query = reference.orderByChild("date").equalTo(date);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                myDataList.clear();
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Data data = snapshot.getValue(Data.class);
+                    myDataList.add(data);
+                }
+
+                todaysItemsAdapter.notifyDataSetChanged();
+
+                int totalAmount = 0;
+                for (DataSnapshot ds : snapshot.getChildren()){
+                    Map<String, Object> map = (Map<String, Object>) ds.getValue();
+                    Object total = map.get("amount");
+                    int pTotal = Integer.parseInt(String.valueOf(total));
+                    totalAmount+=pTotal;
+
+                    amountView.setText("Total Amount on Today's Expense: $"+totalAmount);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void addItemSpentOn() {
@@ -137,13 +198,14 @@ public class Dashboard extends AppCompatActivity {
                         }
                     });
                 }
-                loader.dismiss();
+                dialog.dismiss();
             }
         });
 
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 dialog.dismiss();
             }
         });
---
+++ app/src/main/java/com/example/trackexpense/TodaysItemsAdapter.java
@@ -0,0 +1,80 @@
+package com.example.trackexpense;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TodaysItemsAdapter extends RecyclerView.Adapter<TodaysItemsAdapter.viewHolder> {
+
+    private Context mContext;
+    private List<Data> myDataList;
+
+    public TodaysItemsAdapter(Context mContext, List<Data> myDataList) {
+        this.mContext = mContext;
+        this.myDataList = myDataList;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.retrieve_layout, parent, false);
+        return new TodaysItemsAdapter.viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+        final Data data = myDataList.get(position);
+        holder.item.setText("Item: " + data.getItem());
+        holder.amount.setText("Amount: $" + data.getAmount());
+        holder.date.setText("Today: " + data.getDate());
+        holder.item.setText("Note: " + data.getNotes());
+
+
+        switch (data.getItem()) {
+            case "Transport":
+                holder.imageView.setImageResource(R.drawable.transport);
+                break;
+            case "Food":
+                holder.imageView.setImageResource(R.drawable.food);
+                break;
+            case "Entertainment":
+                holder.imageView.setImageResource(R.drawable.entertainment);
+                break;
+            case "Other":
+                holder.imageView.setImageResource(R.drawable.other);
+                break;
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return myDataList.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder{
+
+        public TextView item, amount, date, notes;
+        public ImageView imageView;
+
+        public viewHolder(@NonNull View itemView){
+            super(itemView);
+
+            item = itemView.findViewById(R.id.item);
+            amount = itemView.findViewById(R.id.amount);
+            date = itemView.findViewById(R.id.date);
+            notes = itemView.findViewById(R.id.note);
+            imageView = itemView.findViewById(R.id.imageView);
+
+        }
+    }
+}
